1d39ed6937a6d376173eb8b7e4c5509972c596a1
Avicenna <maverickbozo@me.com>
2015-10-13 03:15:09 +0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject me.maverickbozo/clj-jwplatform "0.1.0-SNAPSHOT"
+(defproject me.maverickbozo/clj-jwplatform "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [medley "0.7.0"]
                  [clj-time "0.11.0"]
